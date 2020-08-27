a432a6dbf12f965e9f044969c34e49175fe8b930
Saqib Aleem <18554574+saqe@users.noreply.github.com>
2020-08-27 18:22:14 +0500

    Update myfunction.js

--- unctions/myfunction.js
+++ unctions/myfunction.js
@@ -1,6 +1,6 @@
-exports.handler = function(event, context, callback) {
-    callback(null, {
+export async function handler(event, context) {
+  return {
     statusCode: 200,
-    body: "Hello, World"
-    });
+    body: JSON.stringify({ message: `Hello world ${Math.floor(Math.random() * 10)}` })
+  };
 }
