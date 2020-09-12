88e329174fac257bec05d7750956a9c21feb4d0c
SaqibAleem <saqib.py@gmail.com>
2020-09-12 12:44:24 +0500

    updated function

--- functions/myfunction.js
+++ functions/myfunction.js
@@ -4,9 +4,13 @@ var MongoClient = require('mongodb').MongoClient;
 
 exports.handler = (event, context, callback) => {
   console.log('Calling MongoDB Atlas from AWS Lambda with event: ' + JSON.stringify(event));
-  
-  callback(null, {
-    statusCode: 200,
-    body: 'No worries, all is working fine!'
-  })
+  MongoClient.connect(process.env['MONGODB_URI'], function(err, db) {
+    if (err) throw err;
+    // var dbo = db.db("mydb");
+    var myobj = { name: "Company Inc", address: "Highway 37" };
+    callback(null, {
+      statusCode: 200,
+      body: 'No worries, all is working fine!'
+    })
+  });
 }
